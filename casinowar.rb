e3f284e9558a8b29d7766761d716b67399a7cd55
Alexander Campbell <acampbell2231@gmail.com>
2017-08-21 17:57:11 -0600

    I broke casinowar

--- casinowar.rb
+++ casinowar.rb
@@ -4,27 +4,34 @@ require_relative 'deck'
 @player = []
 @dealer = []
 
-class Casinowar
-  def initialize
+class CasinoWar
+  def initialize (player)
     puts "Welcome to casino war"
-    @deck = Deck.new("6")
+    @deck = Deck.new(6, "casinowar")
+    @deck = @deck.shuffle_cards
     puts "How much would you like to bet"
     bet = player.wallet.ask_for_bet
-    result = draw_card(bet)
-    @dealer << draw_card.pop
-    @player << draw_card.pop
+    @user_hand = []
+    @dealer_hand = []
+    @user_hand << @deck.pop
+    @dealer_hand << @deck.pop
+    puts "Player Hand is #{@user_hand[0].display_card}"
+    puts "Dealer_hand is #{@dealer_hand[0].display_card}"
 
-    if @player <= @dealer
+    if @user_hand[0].val > @dealer_hand[0].val
       puts "Player has won"
+      player.wallet.add(bet)
+      puts "You won #{bet}"
     else
       puts "Dealer has won"
+      player.wallet.subtract(bet)
     end
 
   end
 
-  def draw_card
+
+  def draw_card(bet)
     generate_deck
-    shuffle_cards
     draw_card.pop
     puts "You have drawn card "
   end
