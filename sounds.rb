08703498cc6f05abec7d68c4047b0ccbca5862b1
Alexander Campbell <acampbell2231@gmail.com>
2017-08-21 14:20:44 -0600

    More Sounds

--- sounds.rb
+++ sounds.rb
@@ -7,9 +7,12 @@ class Sound
     @welcome = Sounder::Sound.new "assets/welcome.wav"
     @winning = Sounder::Sound.new "assets/winslot.wav"
     @beginslot = Sounder::Sound.new "assets/beginslot.mp3"
+    @lose = Sounder::Sound.new "assets/lose.wav"
+    @no = Sounder::Sound.new "assets/no.wav"
+    @diceroll = Sounder::Sound.new "assets/diceroll.wav"
+    @roulette = Sounder::Sound.new "assets/roulette.wav"
+    @shufflecard = Sounder::Sound.new "assets/shufflecard.wav"
   end
 
-  def winning
-    @winning.play
-  end
+
 end
