e17c5a6caa783d1c20c35fd20494882ade381c0c
Harry Plumer <harry.plumer@gmail.com>
2017-08-21 12:02:06 -0600

    added ask for bet method

--- wallet.rb
+++ wallet.rb
@@ -2,7 +2,6 @@ class Wallet
   attr_accessor :amount
   def initialize(amount)
     @amount = amount
-
   end
 
   def add(money)
@@ -11,6 +10,17 @@ class Wallet
 
   def subtract(money)
     @amount -= money
+  end
+
+  def ask_for_bet()
+    puts "How much would you like to bet?"
+      bet = gets.to_f
+      if bet > @amount
+        puts "Error, you don't have that much! Please choose again."
+        ask_for_bet
+      else
+        bet
+      end
   end
   #TODO: should have other methods to add or sub from wallet
 end
