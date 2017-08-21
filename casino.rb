971024713c3deb199b6aae92cbdec4df5cccd710
Matt Shannon <shannon.matt@outlook.com>
2017-08-21 10:12:47 -0600

    player casino wallet code

---
+++ casino.rb
@@ -0,0 +1,16 @@
+require 'pry'
+
+require_relative 'player'
+
+
+class Casino
+  attr_accessor :players
+
+  def initialize
+    puts 'Welcome To The Ruby Casino'
+    @player = Player.new
+
+  end
+end
+
+Casino.new
