a606306ea7248b5d9ed4cdd88934c2af192b5ba6
Matt Shannon <shannon.matt@outlook.com>
2017-08-21 13:07:35 -0600

    high low game

--- high_low.rb
+++ high_low.rb
@@ -1,12 +1,46 @@
+
+
 class HighLow
   def initialize(player)
     puts "Welcome To High Low: #{player.name}"
     puts "You have #{player.wallet.amount} to bet with!"
+    bet = player.wallet.ask_for_bet
     #Ask player for bet
     #error checking for amount and data type
-    #play game
-    #add or subtract from wallet after game
-    #play again or go back to main menu
+    @first_num = rand(100) + 1
+    @second_num = rand(100) + 1
+    while @second_num == @first_num
+      @second_num = rand(100) + 1
+    end
+
+    puts "Is the next number Higher or Lower than #{@first_num}?"
+      choice = gets.strip
+      while choice.downcase != "higher" && choice.downcase != "lower"
+        puts "Please enter either higher or lower"
+        choice = gets.strip
+      end
+
+      if @first_num < @second_num
+        if choice.downcase == "higher"
+          puts " You win! #{@second_num} is higher than #{@first_num}."
+          player.wallet.add(bet)
+        else
+          puts "You lose! #{@second_num} is lower than #{@first_num}."
+          player.wallet.subtract(bet)
+        end
+      end
+
+      if @first_num > @second_num
+        if choice.downcase == "lower"
+          puts " You win! #{@second_num} is lower than #{@first_num}."
+          player.wallet.add(bet)
+        else
+          puts "You lose! #{@second_num} is higher than #{@first_num}."
+          player.wallet.subtract(bet)
+        end
+      end
+
+
   end
 
 end
