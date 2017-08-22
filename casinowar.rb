6355f9ca04d2aeffc8be4abb7d8dbb5d69b5fe0e
Harry Plumer <harry.plumer@gmail.com>
2017-08-21 21:04:39 -0600

    everything up to date

--- casinowar.rb
+++ casinowar.rb
@@ -7,13 +7,6 @@ class CasinoWar
   def initialize (player)
     @sounds = Sound.new
     @player = player
-<<<<<<< HEAD
-    puts "Welcome to casino war"
-    @deck = Deck.new(6, "casinowar")
-    @deck = @deck.shuffle_cards
-    puts "How much would you like to bet"
-
-=======
     puts "
     
     .o88b.  .d8b.  .d8888. d888888b d8b   db  .d88b.      db   d8b   db  .d8b.  d8888b.
@@ -26,14 +19,11 @@ class CasinoWar
     @deck = Deck.new(6, "casinowar")
     @deck = @deck.shuffle_cards
     puts "How much would you like to bet"
->>>>>>> UI updates and roulette workover
     @bet = @player.wallet.ask_for_bet
     @user_hand = []
     @dealer_hand = []
     @user_hand << @deck.pop
     @dealer_hand << @deck.pop
-<<<<<<< HEAD
-
     evaluate_hand(@user_hand[0], @dealer_hand[0])
   end
 
@@ -43,17 +33,6 @@ class CasinoWar
     str = card.display_card
   end
 
-=======
-    evaluate_hand(@user_hand[0], @dealer_hand[0])
-  end
-
-  def deal_card(card)
-    sleep(1)
-    #add sounds
-    str = card.display_card
-  end
-
->>>>>>> UI updates and roulette workover
   def evaluate_hand(user_card, dealer_card)
     puts "Player Hand is #{deal_card(user_card)}"
     puts "Dealer Hand is #{deal_card(dealer_card)}"
@@ -69,10 +48,6 @@ class CasinoWar
     else
       war_menu
     end
-<<<<<<< HEAD
-
-
-
   end
 
   def war_menu
@@ -91,26 +66,6 @@ class CasinoWar
     end
   end
 
-=======
-  end
-
-  def war_menu
-    puts "Type 1 to go to War and double your bet"
-    puts "Type 2 to surrender and lose half your bet"
-    case gets.to_i
-      when 1
-        start_war
-      when 2
-        loss = @bet / 2
-        puts "You surrender and lose \$#{loss}"
-        @player.wallet.subtract(loss)
-      else
-        puts "Please enter valid input"
-        war_menu
-    end
-  end
-
->>>>>>> UI updates and roulette workover
   def start_war
     puts "You chose war!"
     @bet *= 2
@@ -118,10 +73,5 @@ class CasinoWar
     @dealer_hand << @deck.pop
     evaluate_hand(@user_hand.last, @dealer_hand.last)
   end
-<<<<<<< HEAD
-
-end
-=======
   
 end
->>>>>>> UI updates and roulette workover
